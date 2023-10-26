9892e80bb3be5bf4f78e19e0b21d545d831d99b4
tugwellchristi <tugwellchristi@gmail.com>
2023-10-26 16:23:28 -0700

    additional changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,30 @@
 // Wraps all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(doucment).ready(function() {
+$(doucment).ready(function () {
   // Adds click event listener to the save button
-  $(".saveBtn").on("click", function() {
+  $(".saveBtn").on("click", function () {
     // Gets the parent time block's id
     let timeBlockId = $(this).closest(".time-block").attr("id");
     let userInput = $(this).siblings(".description").val();
   });
 
-    let currentDay = new Date().format(".hour");
+  let currentHour = dayjs().format("H");
 
+  $(".time-block").each(function () {
+    let timeBlockId = $(this).attr("id");
+    let hour = parseInt(timeBlockId.split("-")[1]);
 
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === "currentHour") {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 
-
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
 });
 

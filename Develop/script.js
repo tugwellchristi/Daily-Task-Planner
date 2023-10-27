ba158c0ea8d65ad815cc3ae0d5e57768f96df01b
tugwellchristi <tugwellchristi@gmail.com>
2023-10-27 15:23:16 -0700

    fixed local storage, colors not changing with time

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,34 @@
 // Wraps all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(doucment).ready(function () {
+$(document).ready(function () {
   // Adds click event listener to the save button
   $(".saveBtn").on("click", function () {
     // Gets the parent time block's id
     let timeBlockId = $(this).closest(".time-block").attr("id");
     let userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, userInput);
   });
 
-  let currentHour = dayjs().format("H");
+  let currentHour = dayjs().hour();
+
+
 
   $(".time-block").each(function () {
     let timeBlockId = $(this).attr("id");
     let hour = parseInt(timeBlockId.split("-")[1]);
 
+    let savedInput = localStorage.getItem(timeBlockId);
+    if (savedInput !== null) {
+      $(this).find(".description").val(savedInput);
+    }
+
     if (hour < currentHour) {
-      $(this).addClass("past");
-    } else if (hour === "currentHour") {
-      $(this).addClass("present");
+      $(this).removeClass("present future").addClass("past");
+    } else if (hour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
     } else {
-      $(this).addClass("future");
+      $(this).removeClass("past").addClass("future");
     }
   });
 
